f08a9e9d67fb0dc5765a742d7cfd3730e711ec35
DESKTOP-UPHRIM6\Dell <rajjadav14@gmail.com>
2021-01-19 10:05:48 +0530

    intial

--- server.ts
+++ server.ts
@@ -1,1 +1,6 @@
 console.log("started");
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>console.log("server startes"));
+app.get('/',(req,res)=>res.send('api running'));
